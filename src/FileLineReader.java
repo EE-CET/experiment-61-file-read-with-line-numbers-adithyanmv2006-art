f412d2153f114bd8a1d20291d1161af7ca3ee6e5
Adithyan <adithyanmv2006@gmail.com>
2026-02-20 10:11:19 +0530

    Error corredted

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class FileLineReader {
     public static void main(String[] args) {
-        String fileName = "input.txt";
+        String fileName = "D:\\java\\Experiments\\CYCLE5\\experiment-61-file-read-with-line-numbers-adithyanmv2006-art\\input.txt";
         try (BufferedReader br=new BufferedReader(new FileReader(fileName))){
             String line;
             int lineNumber=1;
