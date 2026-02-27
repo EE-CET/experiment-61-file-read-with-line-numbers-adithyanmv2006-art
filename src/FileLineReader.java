796560fcee2913557b46b4f0e58c3bac7ffa497c
Adithyan <adithyanmv2006@gmail.com>
2026-02-27 10:08:17 +0530

    error corrected

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -4,19 +4,30 @@ import java.io.IOException;
 
 public class FileLineReader {
     public static void main(String[] args) {
-        String fileName = "D:\\java\\Experiments\\CYCLE5\\experiment-61-file-read-with-line-numbers-adithyanmv2006-art\\input.txt";
-        try (BufferedReader br=new BufferedReader(new FileReader(fileName))){
-            String line;
-            int lineNumber=1;
+        String fileName = "input.txt";
+        
+        // TODO: Use a try-with-resources block to open a BufferedReader for the file
+        // TODO: Read the file line by line using a loop
+        // TODO: Keep track of the line number (starting from 1)
+        // TODO: Print each line in the format "lineNumber: lineContent"
+        // TODO: Catch and handle any IOExceptions that might occur
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
 
-            while((line=br.readLine())!=null){
-                System.out.println(lineNumber+": "+line);
-                lineNumber++;
-            }
-        } catch (IOException e) {
-            System.out.println("Error");
-        }
-        
-        
+		String line;
+		int lineNum=1;
+
+		while((line= br.readLine())!=null){
+
+			System.out.println(lineNum+": "+line);
+			lineNum++;
+
+		}
+
+	}catch(IOException e){
+
+		System.out.println("File read error : "+e.getMessage());
+
+	}
     }
 }
